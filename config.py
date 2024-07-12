d1039c3f544a35a1b74517af1389d4a2d9197d4f
Ayjemal07 <125630176+Ayjemal07@users.noreply.github.com>
2024-07-12 13:37:45 -0700

    added configs for other api keys

--- config.py
+++ config.py
@@ -11,6 +11,10 @@ class Config:
     # Secret key for flask session management
     SECRET_KEY = os.getenv('FLASK_SESSION')
 
+    # Other API keys
+    OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+    YOUTUBE_API_KEY = os.getenv('YOUTUBE_API_KEY')
+
 
         # Set session timeout (example: 30 minutes)
     PERMANENT_SESSION_LIFETIME = 60  # 1800 seconds = 30 minutes, do 1 min for now(for testing)
